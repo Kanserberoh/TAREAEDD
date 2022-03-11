f85f1621d460f4c473dd3e3efed70e86796b2fa5
DESKTOP-AEDD91U\Usuario <politotuto@gmail.com>
2022-03-11 21:36:55 +0100

    refactor: se ha realizado una extracción del método statement al método amountFor, que permite distinguir entre Movie.REGULAR, Movie.NEW_RELEASE y Movie.CHILDRENS.

--- proyecto/src/main/java/Customer.java
+++ proyecto/src/main/java/Customer.java
@@ -21,24 +21,7 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
+            
             // add frequent renter points
             frequentRenterPoints ++;
             // add bounus for a two day new release rental
@@ -56,5 +39,24 @@ public class Customer {
 
     public String getName() {
         return _name;
+    }
+    public double amountFor(Rental each, double thisAmount){
+                // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+return thisAmount;
     }
 }

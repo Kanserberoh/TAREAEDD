dfd7a53a119363c12cb228468aceea2b91b47acf
DESKTOP-AEDD91U\Usuario <politotuto@gmail.com>
2022-03-17 19:32:21 +0100

    Refactor: Se ha movido el método amountFor de la clase Customer a la clase Rental, se ha renombrado como *getCharge()** y se ha establecido como método privado.

--- proyecto/src/main/java/Customer.java
+++ proyecto/src/main/java/Customer.java
@@ -44,23 +44,5 @@ public class Customer {
                 frequentRenterPoints ++;
    return frequentRenterPoints;
    }
-    public double amountFor(Rental each, double thisAmount){
-                // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-return thisAmount;
-    }
+
 }
